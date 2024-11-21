3f5892debffe394633b11347a68e104c0bf54df9
shreesha-j <shreesha1005@gmail.com>
2024-11-21 04:17:42 +0000

    - Disabled default body parsing for the Next.js API to handle custom file uploads. - Set experimental server actions with increased body size limit (1000MB).

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  api: {
+    bodyParser: false,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '1000mb',
+    },
+  },
 };
 
 export default nextConfig;
